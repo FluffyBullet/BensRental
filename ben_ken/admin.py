8b750687947528cb40fe0834d84a2abd0ba87b4f
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-01-26 15:52:53 +0000

    creation of Booking and Profile db_tables

--- ben_ken/admin.py
+++ ben_ken/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from .models import Booking, Profile
+
+admin.site.register(Profile)
+admin.site.register(Booking)
--- ben_ken/models.py
+++ ben_ken/models.py
@@ -6,3 +6,21 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0,"Display"),(1,"Hide"))
 
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contact_number = models.CharField(max_length=11, blank=False)
+    address_line_1 = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=7)
+      
+    def __str__(self):
+        return self.user
+
+class Booking(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    booking_reference = models.AutoField(unique=True, primary_key=True)
+    week_booking = models.SmallIntegerField()
+    year_booking = models.SmallIntegerField()
+    calendar_reference = models.SmallIntegerField(unique=True)
+    number_of_o18 = models.SmallIntegerField(blank=False)
+    number_of_u18 = models.SmallIntegerField(blank=False)
+    number_of_pets = models.SmallIntegerField(blank=False)

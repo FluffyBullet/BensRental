c121e6e97a08d0b1da5a41b99a87db88d2a8a288
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-02-15 21:46:53 +0000

    addition to view private comments and if avaialble on booking

--- ben_ken/admin.py
+++ ben_ken/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Booking, Profile, OverallComment
+from .models import Booking, Profile, OverallComment, PersonalComment
 
 admin.site.register(Profile)
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('booking_reference','week_booking','year_booking',)
-    list_filter = ('week_booking','year_booking')
+    list_display = ('booking_reference','week_booking','year_booking','if_available')
+    list_filter = ('week_booking','year_booking', 'if_available')
 
 admin.site.register(Booking, BookingAdmin)
 
@@ -14,3 +14,5 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ('overall_feeling',)
 
 admin.site.register(OverallComment, CommentAdmin)
+
+admin.site.register(PersonalComment, CommentAdmin)

dd7e9a990e68ed7e32d6056f5d94b00cbd3ffa8b
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-02-16 11:15:55 +0000

    turning off of debug in settings

--- caravan_rental/settings.py
+++ caravan_rental/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'static/templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bensrental.herokuapp.com', 'localhost', '127.0.0.1']
 

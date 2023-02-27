9d3b118ed2b1697d15bbdbdc52b8c32195246625
FluffyBullet <davidreynolds34@hotmail.co.uk>
2023-02-27 19:55:56 +0000

    beautified models.py by tutorialspoint.com

--- ben_ken/models.py
+++ ben_ken/models.py
@@ -4,35 +4,40 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
-STATUS = ((0,"Display"),(1,"Hide"))
+STATUS = ((0, 'Display'), (1, 'Hide'))
 
 
 class Profile(models.Model):
-    profile_reference = models.IntegerField(auto_created=True, primary_key=True, unique=True)
-    user_id = models.ForeignKey(User, to_field="username", on_delete=models.DO_NOTHING)
+
+    profile_reference = models.IntegerField(auto_created=True,
+            primary_key=True, unique=True)
+    user_id = models.ForeignKey(User, to_field='username',
+                                on_delete=models.DO_NOTHING)
     contact_number = models.CharField(max_length=11, blank=False)
     address_line_1 = models.CharField(max_length=50)
     address_line_2 = models.CharField(max_length=50)
     address_line_3 = models.CharField(max_length=50)
     address_line_4 = models.CharField(max_length=50)
     postcode = models.CharField(max_length=7)
-    phone = "phone"
-    email = "email"
-    post = "post"
-    preference_of_contact = models.CharField(max_length=6,choices=(
-        (phone,"Phone"),
-        (post, "Post"),
-        (post, "letter"),
-    ))
+    phone = 'phone'
+    email = 'email'
+    post = 'post'
+    preference_of_contact = models.CharField(max_length=6,
+            choices=((phone, 'Phone'), (post, 'Post'), (post, 'letter'
+            )))
     admin_comments = models.CharField(max_length=250, blank=True)
-      
+
     def __str__(self):
         return self.user_id.username
 
 
 class Booking(models.Model):
-    booking_reference = models.IntegerField(unique=True, primary_key=True, null=False)
-    booker = models.ForeignKey(User, to_field="username", on_delete=models.DO_NOTHING,blank=True, null=True)
+
+    booking_reference = models.IntegerField(unique=True,
+            primary_key=True, null=False)
+    booker = models.ForeignKey(User, to_field='username',
+                               on_delete=models.DO_NOTHING, blank=True,
+                               null=True)
     week_booking = models.SmallIntegerField()
     year_booking = models.SmallIntegerField()
     if_available = models.BooleanField(default=True)
@@ -41,51 +46,51 @@ class Booking(models.Model):
     number_of_pets = models.SmallIntegerField(default=0)
     charge = models.PositiveSmallIntegerField(null=True)
     additional_comment = models.TextField(blank=True, null=True)
-    provided_number = models.CharField(max_length=11, blank=True, null=True)
+    provided_number = models.CharField(max_length=11, blank=True,
+            null=True)
 
     def __str__(self):
-        return "Booking reference: " + str(self.booking_reference)
- 
+        return 'Booking reference: ' + str(self.booking_reference)
+
 
 class OverallComment(models.Model):
+
     o_comment_id = models.AutoField(primary_key=True, unique=True)
-    booking_reference = models.OneToOneField(Booking, to_field='booking_reference', on_delete=models.CASCADE)
+    booking_reference = models.OneToOneField(Booking,
+            to_field='booking_reference', on_delete=models.CASCADE)
     comment = models.CharField(max_length=250)
-    happy ="happy"
-    indifferent = "indifferent"
-    unhappy ="unhappy"
-    overall_feeling = models.CharField(max_length=11,
-        choices=(
-        (happy, "happy"),
-        (indifferent, "indifferent"),
-        (unhappy, "unhappy"),
-        )
-    )
+    happy = 'happy'
+    indifferent = 'indifferent'
+    unhappy = 'unhappy'
+    overall_feeling = models.CharField(max_length=11, choices=((happy,
+            'happy'), (indifferent, 'indifferent'), (unhappy, 'unhappy'
+            )))
 
     def __int__(self):
         return self.o_comment_id
 
     def __str__(self):
-        return "comment" + str(self.o_comment_id) + "by" + str(self.booking_reference.booker) 
-    
-class PersonalComment(models.Model):
-    p_comment_id = models.AutoField(primary_key=True, unique=True)
-    booking_reference = models.ForeignKey(Booking, to_field="booking_reference", on_delete=models.CASCADE)
-    personal_comment = models.CharField(max_length=250)
-    happy ="Happy"
-    indifferent = "Indifferent"
-    unhappy ="Unhappy"
+        return 'comment' + str(self.o_comment_id) + 'by' \
+            + str(self.booking_reference.booker)
 
-    overall_feeling = models.CharField(max_length=11,
-        choices=(
-        (happy, "Happy"),
-        (indifferent, "Indifferent"),
-        (unhappy, "Unhappy"),
-        )
-    )
+
+class PersonalComment(models.Model):
+
+    p_comment_id = models.AutoField(primary_key=True, unique=True)
+    booking_reference = models.ForeignKey(Booking,
+            to_field='booking_reference', on_delete=models.CASCADE)
+    personal_comment = models.CharField(max_length=250)
+    happy = 'Happy'
+    indifferent = 'Indifferent'
+    unhappy = 'Unhappy'
+
+    overall_feeling = models.CharField(max_length=11, choices=((happy,
+            'Happy'), (indifferent, 'Indifferent'), (unhappy, 'Unhappy'
+            )))
 
     def __int__(self):
         return self.p_comment_id
 
     def __str__(self):
-        return "comment" + str(self.p_comment_id) + "by" + str(self.booking_reference.booker) 
+        return 'comment' + str(self.p_comment_id) + 'by' \
+            + str(self.booking_reference.booker)
